067a532809d9ec4268cbdc50c0ca8847eaa83b50
AgathaBssn <agatha.basson@gmail.com>
2024-03-19 19:46:56 +0100

    init

--- learnTS/dist/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const compteur = document.querySelector('#compteur');
-let i = 0;
-const increment = (e) => {
-    i++;
-    compteur.querySelector('span').innerText = i.toString();
-};
-compteur.addEventListener('click', increment);
--- notesIUT/main.js
+++
@@ -1,3 +0,0 @@
-var header = document.querySelector("header");
-var section = document.querySelector("section");
-import data from './model.json' assert { type: 'json' }
